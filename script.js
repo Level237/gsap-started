389957a0686e062cde22f13a092644c806195647
level237 <Bramslevel129@gmail.com>
2024-07-02 14:28:14 +0100

    scrolltrigger problem

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-var tl=gsap.timeline();
-
-tl.from("h2",{
-    y:-30,
-    opacity:0,
-    duration:1,
-    delay:0.5
+gsap.from("#page1 #box", {
+    scale:0,
+    duration: 2,
+    rotate:360
 })
 
-tl.from("h4",{
-    y:-30,
-    opacity:0,
-    duration:1,
-    stagger:0.3
-})
-
-tl.from("h1",{
-    y:20,
-    opacity:0,
-    duration:1,
-    scale:0.3
+gsap.from("#page2 #box", {
+    scale:0,
+    duration: 2,
+    rotate:360
 })
