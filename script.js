e5fb6627d265a39aee1e78f3ced48e0c9b00ce95
level237 <Bramslevel129@gmail.com>
2024-07-02 13:36:58 +0100

    timeline in gsap

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-gsap.to("#box",{
-    x:1200,
-    duration:1,
-    delay:1,
+//gsap.to("#box1",{
+    //x:1200,
+    //rotate:360,
+    //duration:0.77,
+    //delay:0.5
+//})
+//gsap.to("#box2",{
+    //x:1200,
+    //backgroundColor:"yellow",
+    //duration:1.5,
+    //delay:2.5
+//})
+//gsap.to('#box3',{
+    //x:1200,
+    //scale:0.5,
+    //borderRadius:"50%",
+    //duration:1.5,
+   // delay:4
+//})
+
+var tl=gsap.timeline()
+
+tl.to("#box1",{
+    x:1500,
     rotate:360,
-    repeat:-1,
-    yoyo:true
+    duration:1.5,
+    delay:1
+})
+
+tl.to("#box2",{
+    x:1500,
+    duration:1.5
+})
+
+tl.to("#box3",{
+    x:1500,
+    duration:1.5
 })
